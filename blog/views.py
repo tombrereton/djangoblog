9d9e407e6f569de74b3ca0d5d5d6f5762d8b3dfd
tombrereton <tbererton89@gmail.com>
2016-05-19 23:46:22 +0100

    Changed order of posts to latest first.

--- blog/views.py
+++ blog/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth.decorators import login_required
 
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('created_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-created_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 
